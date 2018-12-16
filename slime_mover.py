a7e29b1daa99214093e0279989dc7e273eec5c8e
Ben Humphries <benfhumphries@gmail.com>
2018-12-16 18:21:52 -0500

    Basic pygame window

---
+++ slime_mover.py
@@ -0,0 +1,21 @@
+import pygame, sys
+
+
+pygame.init()
+
+size = width, height = 800, 600
+BLACK = (0,0,0)
+
+screen = pygame.display.set_mode(size)
+
+while 1:
+
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			pygame.display.quit()
+			pygame.quit()
+			sys.exit()
+
+	screen.fill(BLACK)
+	
+	pygame.display.flip()
