a943481307fb0c79688ebe2312deca705fdb1dc0
Ben Humphries <benfhumphries@gmail.com>
2018-12-17 13:57:33 -0500

    Corrected weights so that there is now a weight from every node to every other node, not solely 1 : 1

--- neural_network.py
+++ neural_network.py
@@ -3,12 +3,17 @@ import numpy as np
 
 class NeuralNetwork():
 
+	# weights = [i1->o, i2->o]
+	# i1->o = [->o1, ->o2]
+	# i2->o = [->o1, ->o2]
 
-	def __init__(self, weights = [random.uniform(-1, 1), random.uniform(-1, 1)]):
+	bias = 1
+
+	def __init__(self, weights = [[random.uniform(-1, 1), random.uniform(-1, 1)], [random.uniform(-1, 1), random.uniform(-1, 1)]]):
 		self.weights = weights
 
 	def randomize_weights(self):
-		self.weights = [random.uniform(-1, 1), random.uniform(-1, 1)]
+		self.weights = [[random.uniform(-1, 1), random.uniform(-1, 1)], [random.uniform(-1, 1), random.uniform(-1, 1)]]
 
 	def set_weights(self, weights):
 		self.weights = weights
@@ -18,6 +23,9 @@ class NeuralNetwork():
 
 	def think(self, inputs):
 
-		outputs = [self.sigmoid(i * w + 2) for i, w in zip(inputs, self.weights)]
+		o1 = self.sigmoid(inputs[0] * self.weights[0][0] + inputs[1] * self.weights[1][0] + self.bias)
+		o2 = self.sigmoid(inputs[0] * self.weights[0][1] + inputs[1] * self.weights[1][1] + self.bias)
+
+		outputs = [o1, o2]
 
 		return outputs
